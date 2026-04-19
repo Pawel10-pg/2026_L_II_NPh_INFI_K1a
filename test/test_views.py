507f904b596d574fde15e3c5d44a27806d901773
Paweł10-str. <pgalik02@wp.pl>
2026-04-19 10:40:47 +0200

    Naprawa kodowania UTF-8 w testach - wymuszenie przez cat

--- test/test_views.py
+++ test/test_views.py
@@ -1,18 +1,10 @@
 import unittest
 from hello_world import app
-from hello_world.formater import SUPPORTED
 
-
-class FlaskrTestCase(unittest.TestCase):
+class TestViews(unittest.TestCase):
     def setUp(self):
-        app.config['TESTING'] = True
         self.app = app.test_client()
 
-    def test_outputs(self):
-        rv = self.app.get('/outputs')
-        s = str(rv.data)
-        ','.join(SUPPORTED) in s
-
-    def test_msg_with_output(self):
-        rv = self.app.get('/?output=json')
+    def test_msg_with_name(self):
+        rv = self.app.get('/?name=Paweł')
         self.assertEqual('{ "imie":"Paweł", "msg":"Hello World!"}'.encode('utf-8'), rv.data)
